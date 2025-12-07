5e9167bf86b2f83e8f4817a9f8a3477704fff9dc
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-12-07 14:09:41 +0530

    expense project

--- backend.sh
+++ backend.sh
@@ -29,3 +29,52 @@ VALIDATE()
         echo -e "$2...$G SUCCESS $N"
     fi
 }
+
+cp $SCRIPT_DIR/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "Copy systemctl service"
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disable nodejs"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enable nodejs"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "install nodejs"
+
+id roboshop
+if [ $? -ne 0 ]; then
+   echo "creating roboshop user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating system user"
+else
+    echo -e "roboshop user is already created.....$Y SKIPPING $N"
+fi  
+
+mkdir -p /app 
+VALIDATE $? "create app folder"
+
+curl -o curl -o /tmp/backend.zip https://expense-joindevops.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "unzip backend"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
+
+dnf install mysql -y $>>$LOG_FILE
+VALIDATE $? "install mysql server"
+
+mysql -h mysql.awsdevops.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+
+
+systemctl daemon-reload
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enable backend"
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "restart backend"
